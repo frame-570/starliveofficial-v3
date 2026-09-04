76ed4d5cfdafe493833582804bd65a5a1bcea37c
frame-570 <framef570@gmail.com>
2026-09-04 22:20:24 +0700

    Delete js directory

--- js/config.js
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = "https://zclefxrshkeyozprnpfb.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_GLwQqBTzFDn17Dncuj2XBA_irN8TWB4";
-export const FUNCTIONS_URL = `${SUPABASE_URL}/functions/v1`;
